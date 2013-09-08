d32da379731547e1ef5b749160716807df6ed697
Josef Holzmayr <holzmayr@rsi-elektrotechnik.de>
2013-09-08 13:01:35 +0200

    demo integer parameter

--- kmw.c
+++ kmw.c
@@ -2,18 +2,21 @@
 
 #include "kmw.h"
 
+static int kmw_demo_param = 0;
+module_param(kmw_demo_param, int, 0644);
+
 // defines for device infrastructure
 #define		KMWNAME			"kmw"
 
 static int kmw_init(void)
 {
-	PDEBUG("init\n");
+	PDEBUG("init - demo param: %d\n", kmw_demo_param);
 	return 0;
 }
 
 static void kmw_exit(void)
 {
-	PDEBUG("exit\n");
+	PDEBUG("exit - demo param: %d\n", kmw_demo_param);
 }
 
 module_init(kmw_init);
