8d1177243bfcc22c328b5c15643596174d4a6a89
Josef Holzmayr <holzmayr@rsi-elektrotechnik.de>
2013-09-10 19:12:16 +0000

    add support for bacon cape gpios

--- kmw-device.c
+++ kmw-device.c
@@ -1,3 +1,4 @@
+#include <linux/gpio.h>
 #include <linux/module.h>
 #include <linux/platform_device.h>
 
@@ -28,7 +29,12 @@ void	kmw_set_output(unsigned int value)
 {
 	int i;
 	for (i = 0; i < KMW_DEV_LED_COUNT; ++i)
+	{
 		PDEBUG("device - set led%d @ gpio %u to %s\n", i, *kmw_dev_leds[i], ((value & (1L << i)) ? "high" : "low"));
+#ifdef CONFIG_GPIOLIB
+		gpio_direction_output(*kmw_dev_leds[i], (value & (1L << i)));
+#endif
+	}
 }
 
 static struct kmw_ops kmw_device_ops = {
@@ -48,10 +54,27 @@ static int kmw_device_init(void)
 	int err;
 
 	PDEBUG("device_init\n");
-	err = platform_device_register(&kmw_device);
+#ifdef CONFIG_GPIOLIB
+	err = gpio_request(kmw_dev_led_red, KMWNAME "_led_red");
 	if (err < 0)
 		goto err1;
+	err = gpio_request(kmw_dev_led_green, KMWNAME "_led_green");
+	if (err < 0)
+		goto err2;
+	err = gpio_request(kmw_dev_led_blue, KMWNAME "_led_blue");
+	if (err < 0)
+		goto err3;
+#endif
+	err = platform_device_register(&kmw_device);
+	if (err < 0)
+		goto err4;
 	return 0;
+err4:
+	gpio_free(kmw_dev_led_blue);
+err3:
+	gpio_free(kmw_dev_led_green);
+err2:
+	gpio_free(kmw_dev_led_red);
 err1:
 	return err;
 }
@@ -59,6 +82,11 @@ err1:
 static void kmw_device_exit(void)
 {
 	PDEBUG("device_exit\n");
+#ifdef CONFIG_GPIOLIB
+	gpio_free(kmw_dev_led_red);
+	gpio_free(kmw_dev_led_green);
+	gpio_free(kmw_dev_led_blue);
+#endif
 	platform_device_unregister(&kmw_device);
 }
 
