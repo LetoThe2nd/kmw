aeca87f7b8401b34e5182b1b86822940234cbf79
Josef Holzmayr <holzmayr@rsi-elektrotechnik.de>
2013-09-10 19:19:59 +0200

    add gpio addresses

--- kmw-device.c
+++ kmw-device.c
@@ -3,6 +3,22 @@
 
 #include "kmw.h"
 
+static unsigned int kmw_dev_led_red = 7;
+static unsigned int kmw_dev_led_green = 50;
+static unsigned int kmw_dev_led_blue = 51;
+
+module_param(kmw_dev_led_red, uint, 0644);
+module_param(kmw_dev_led_green, uint, 0644);
+module_param(kmw_dev_led_blue, uint, 0644);
+
+static unsigned int * const kmw_dev_leds[] = {
+	&kmw_dev_led_red,
+	&kmw_dev_led_green,
+	&kmw_dev_led_blue,
+};
+
+#define KMW_DEV_LED_COUNT (sizeof(kmw_dev_leds)/sizeof(unsigned int *))
+
 void    kmw_release(struct device *dev)
 {
 	PDEBUG("device release\n");
@@ -11,8 +27,8 @@ void    kmw_release(struct device *dev)
 void	kmw_set_output(unsigned int value)
 {
 	int i;
-	for (i = 0; i < 3; ++i)
-		PDEBUG("device - set led%d to %s\n", i, ((value & (1L << i)) ? "high" : "low"));
+	for (i = 0; i < KMW_DEV_LED_COUNT; ++i)
+		PDEBUG("device - set led%d @ gpio %u to %s\n", i, *kmw_dev_leds[i], ((value & (1L << i)) ? "high" : "low"));
 }
 
 static struct kmw_ops kmw_device_ops = {
